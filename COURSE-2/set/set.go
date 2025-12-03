0f95b2bec6a160b6e359298edf43865353bfc98c
Eugene Kozlovsky <eu.new.gene@gmail.com>
2025-12-03 17:37:53 +0300

    fix: following code convention

--- COURSE-2/set/set.go
+++ COURSE-2/set/set.go
@@ -6,9 +6,9 @@ func UniqueStrings(ss []string) []string {
 
 	// For every el of given slice check wether it was already met as key
 	for _, s := range ss {
-		_, ok := set[s]
+		
 		// If slot for current key was empty fullfil it with struct{}{} and append to slice of unique elems
-		if !ok { 
+		if _, ok := set[s]; !ok { 
 			set[s] = struct{}{}
 			uniqueRes = append(uniqueRes, s)
 		}
