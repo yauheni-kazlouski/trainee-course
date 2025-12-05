a54be048daa4f576ad7d43cb3025938d3cbe4e77
Eugene Kozlovsky <eu.new.gene@gmail.com>
2025-12-05 19:23:42 +0300

    fix: code was completely changed in order to implement gracefull shutdown correctly

--- COURSE-5/main.go
+++ COURSE-5/main.go
@@ -17,159 +17,106 @@ type Cake struct {
 	PackTime int
 }
 
-type BakersPool struct{
-	jobs	chan struct{}
-	result 	chan Cake
-	t1		int
-	wg		sync.WaitGroup
-}
-
-func NewBakersPool(numBakers, numJobs int) *BakersPool {
-	pool := &BakersPool{
-		jobs: make(chan struct{}, numJobs),
-		result: make(chan Cake, numBakers),
-		t1: 2,
-	}
-
-	pool.wg.Add(numBakers)
-	for i := range(numBakers){
-		go pool.worker(i)
-	}
-	return pool
-}
-
-func (pool *BakersPool) worker(i int) {
-	defer pool.wg.Done()
-	for range pool.jobs {
-		time.Sleep(time.Duration(i + pool.t1) * time.Millisecond)
-		pool.result <- Cake{BakedBy: i, BakeTime: i + pool.t1}
-	}
-}
-
-func (pool *BakersPool) AddJob(){
-	pool.jobs <- struct{}{}
-}
-
-func (pool *BakersPool) Close() {
-	close(pool.jobs)
-	pool.wg.Wait()
-	close(pool.result)
-}
-
-
-
-
-
-type PackersPool struct{
-	jobs	chan Cake
-	result 	chan Cake
-	t2		int
-	wg		sync.WaitGroup
-}
-
-func NewPackersPool(numPackers, numJobs int) *PackersPool {
-	pool := &PackersPool{
-		jobs: make(chan Cake, numJobs),
-		result: make(chan Cake, numPackers), 
-		t2: 3,
-	}
-
-	pool.wg.Add(numPackers)
-	for i := range numPackers {
-		go pool.worker(i)
-	}
-
-	return pool
-}
-
-func (pool *PackersPool) worker(i int) {
-	defer pool.wg.Done()
-
-	for cake := range pool.jobs {
-		time.Sleep(time.Duration(i + pool.t2) * time.Millisecond)
-		cake.PackedBy = i
-		cake.PackTime = i + pool.t2
-		pool.result <- cake
-	}
-}
-
-func (pool *PackersPool) AddJob(cake Cake){
-	pool.jobs <- cake
-}
-
-func (pool *PackersPool) Close() {
-	close(pool.jobs)
-	pool.wg.Wait()
-	close(pool.result)
-}
-
 func main() {
 	
 	n := 5
-	m := 10
-	k := 10000
-	var poolsWG sync.WaitGroup
-	
-	bakers := NewBakersPool(n, k)
-	packers := NewPackersPool(m, k)
+	m := 15
+	k := 1000
+	t1 := 2
+	t2 := 1
+
+	bakerChan := make(chan struct{}, k)
+	packerChan := make(chan Cake, k)
+	result := make(chan Cake, k)
+	done := make(chan struct{})
 
 	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	go func(){
+		defer close(bakerChan)
+		for range k{
+			select{
+			case <-ctx.Done():
+				return
+			default:
+				bakerChan <- struct{}{}
+			}
+		}
+	}()
 	
-	poolsWG.Add(1)
-	go func(){
-		defer poolsWG.Done()
-		defer bakers.Close()
-		for range k {
-			select{
-			case <-ctx.Done():
-				return
-			default:
-				bakers.AddJob()
+	bakerWg := sync.WaitGroup{}
+	bakerWg.Add(n)
+	for i := range(n) {
+		go func (workerId int) {
+			defer bakerWg.Done()
+			for {
+				select{
+				case _, ok := <- bakerChan:
+					if ok{
+						time.Sleep(time.Duration(workerId + t1) * time.Millisecond)
+						packerChan <- Cake{BakedBy: workerId, BakeTime: workerId + t1}
+					} else {
+						return
+					}
+				case <- ctx.Done():
+					return
+				}
 			}
-		}
+		}(i)
+	}
+	go func() {
+		bakerWg.Wait()
+		close(packerChan)
 	}()
 
-	poolsWG.Add(1)
-	go func(){
-		defer poolsWG.Done()
-		defer packers.Close()
-
-		for cake := range bakers.result {
-			select{
-			case <-ctx.Done():
-				return
-			default:
-				packers.AddJob(cake)
+	packerWg := sync.WaitGroup{}
+	packerWg.Add(m)
+	for i := range(m) {
+		go func (workerId int) {
+			defer packerWg.Done()
+			for {
+				select{
+				case cake, ok := <- packerChan:
+					if !ok {
+						return
+					}
+					time.Sleep(time.Duration(workerId + t2) * time.Millisecond)
+					cake.PackedBy = workerId
+					cake.PackTime = workerId + t2
+					result <- cake
+				case <-ctx.Done():
+					return
+				}
 			}
-		}
+		}(i)
+	}
+	go func() {
+		packerWg.Wait()
+		close(result)
 	}()
 
-	poolsWG.Add(1)
-	go func(){
-		for cake := range packers.result {
+	// I think there's no need to select ctx.Done in this gorouting, 
+	// because shutting down gracefully programm must print all the cakes 
+	// that have been already done and are waiting in the result chan buffer to be displayed
+	go func() {
+		defer close(done)
+		for cake := range result{
 			fmt.Printf("%#v\n", cake)
 		}
-		poolsWG.Done()
 	}()
 
 	sigChan := make(chan os.Signal, 1)
-    signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
-
-	done := make(chan struct{})
-
-	go func(){
-		poolsWG.Wait()
-		close(done)
-	}()
-
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	
 	select {
-	case <-sigChan:
-		fmt.Println("shutting down, waiting to finish goroutings")
 	case <-done:
-		fmt.Println("All done")
+		fmt.Printf("\nprogramm finished successfully\n")
+	case <-sigChan:
+		fmt.Printf("\nprogramm interrupted, shutting down...\n\n")
+		cancel()
+		<-done
 	}
-
-	cancel()
-	poolsWG.Wait()
-	fmt.Println("programm is finished")
+	
+	fmt.Println("\nprogram ended")
 }
